e54ee78920e30e44dd963fb2183185d034bfd23f
Rene <rene66613@hotmail.com>
2013-02-05 18:40:42 +0100

    cleaned some

--- assignment1/basic_midpoint/mla.c
+++ assignment1/basic_midpoint/mla.c
@@ -4,10 +4,10 @@
  * Description ..... Midpoint Line Algorithm
  * Created by ...... Jurgen Sturm 
  *
- * Student name ....
- * Student email ... 
- * Collegekaart ....
- * Date ............
+ * Student names: Tom Peerdeman & Rene Aparicio Saez
+ * Student emails: rene.apariciosaez@student.uva.nl
+ * Collegekaart 10266186 & 10214054
+ * Date 05 Feb 2013
  * Comments ........
  *
  *
@@ -34,44 +34,30 @@
  *
  */
 void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
-  int ix,iy;
+  // variables for the function
   int x,y;
   int x_check;
   int y_check;
   int total_check;
   
-  PutPixel(s,x0,y0,colour);
-  PutPixel(s,x1,y1,colour);
-  
+  // initialize checks
   x_check = x1 - x0;
   y_check = y1 - y0;
   total_check = abs(x_check) - abs(y_check);
   
+  // set buffers
   x=x0;
   y=y0;
-    
-  // if the y change is zero, draw a horizontal line
-  if(y_check == 0){
-    if(x1>x0) ix=1; else ix=-1;
-    for(;x!=x1;x+=ix)
-      PutPixel(s,x,y0,colour);
-  }
-  
-  // if the x change is zero, draw a vertical line
-  else if(x_check == 0){
-    if(y1>y0) iy=1; else iy=-1;
-    for(;y!=y1;y+=iy)
-      PutPixel(s,x1,y,colour); 
-  }
+
   
   // if the x change is greater than zero, draw to the right
-  else if(x_check > 0){
+  if(x_check > 0){
     
     // if the y change is greater than zero, draw down
-    if(y_check > 0){
+    if(y_check >= 0){
       
       // draw red octant
-      if(total_check >= 0){
+      if(total_check > 0){
         for(;x!=x1;x+=1){
           PutPixel(s,x,y,colour);
           if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
@@ -94,10 +80,10 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
     else if(y_check < 0){
       
       // draw white octant
-      if(total_check >= 0){
+      if(total_check > 0){
         for(;x!=x1;x+=1){
           PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) >= 0)
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
             y--;
         }
       }
@@ -106,21 +92,19 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
       else{
         for(;y!=y1;y-=1){
           PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) <= 0)
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
             x++;
         }      
       }
-      
     }
   }
-  
   // if the x change is smaller than zero, draw to the left
-  else if(x_check < 0){
+  else if(x_check <= 0){
     // if the y change is greater than zero, draw down
     if(y_check > 0){
       
       // draw white octant
-      if(total_check > 0){
+      if(total_check >= 0){
         for(;x!=x1;x-=1){
           PutPixel(s,x,y,colour);
           if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
@@ -132,21 +116,20 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
       else{
         for(;y!=y1;y+=1){
           PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) <= 0)
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
             x--;
         }
       }
     }
     
-    
     // if the y change is greater than zero, draw up
-    if(y_check < 0){
+    else if(y_check <= 0){
       
       // draw red octant
       if(total_check > 0){
         for(;x!=x1;x-=1){
           PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) <= 0)
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
             y--;
         }
       }
@@ -155,7 +138,7 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
       else{
         for(;y!=y1;y-=1){
           PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) >= 0)
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
             x--;
         }
       }
