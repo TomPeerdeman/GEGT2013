7c1a675d30f7d5435f6fb06ffd6ac14d38d1efc0
Rene <rene66613@hotmail.com>
2013-02-05 17:13:50 +0100

    now halfway there

--- assignment1/basic_midpoint/mla.c
+++ assignment1/basic_midpoint/mla.c
@@ -47,34 +47,35 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
   y_check = y1 - y0;
   total_check = abs(x_check) - abs(y_check);
   
+  x=x0;
+  y=y0;
+    
   // if the y change is zero, draw a horizontal line
   if(y_check == 0){
     if(x1>x0) ix=1; else ix=-1;
-    for(x=x0;x!=x1;x+=ix)
+    for(;x!=x1;x+=ix)
       PutPixel(s,x,y0,colour);
   }
   
   // if the x change is zero, draw a vertical line
   else if(x_check == 0){
     if(y1>y0) iy=1; else iy=-1;
-    for(y=y0;y!=y1;y+=iy)
+    for(;y!=y1;y+=iy)
       PutPixel(s,x1,y,colour); 
   }
   
   // if the x change is greater than zero, draw to the right
   else if(x_check > 0){
+    
     // if the y change is greater than zero, draw down
     if(y_check > 0){
-      
     }
     
     
     // if the y change is smaller than zero, draw up
-    if(y_check < 0){
-      x=x0;
-      y=y0;
-      printf("\ntotal: %d",total_check);
-      printf("\nx,y: %d;%d, x1,y1: %d;%d", x,y, x1,y1);
+    else if(y_check < 0){
+      
+      // draw white octant
       if(total_check >= 0){
         for(;x!=x1;x+=1){
           PutPixel(s,x,y,colour);
@@ -82,14 +83,16 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
             y--;
         }
       }
+      
+      // draw blue octant
       else{
         for(;y!=y1;y-=1){
           PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) <= 0)
             x++;
         }      
       }
-      printf("\nx,y: %d;%d, x1,y1: %d;%d\n", x,y, x1,y1);
+      
     }
   }
   
@@ -103,11 +106,26 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
     
     // if the y change is greater than zero, draw up
     if(y_check < 0){
-    
+      
+      // draw red octant
+      if(total_check > 0){
+        for(;x!=x1;x-=1){
+          PutPixel(s,x,y,colour);
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) <= 0)
+            y--;
+        }
+      }
+      
+      // draw green octant
+      else{
+        for(;y!=y1;y-=1){
+          PutPixel(s,x,y,colour);
+          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) >= 0)
+            x--;
+        }
+      }
     }
   }
-  
-
   return;
 }
 
