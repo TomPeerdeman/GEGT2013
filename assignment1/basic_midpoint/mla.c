ed0f4cc93f18ce86b4a9ff7d63323ae0b14e0487
Rene <rene66613@hotmail.com>
2013-02-05 14:46:41 +0100

    its something

--- assignment1/basic_midpoint/mla.c
+++ assignment1/basic_midpoint/mla.c
@@ -36,17 +36,63 @@
 void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
   int ix,iy;
   int x,y;
+  int x_check;
+  int y_check;
+  int total_check;
   
   PutPixel(s,x0,y0,colour);
   PutPixel(s,x1,y1,colour);
   
-  if(x1>x0) ix=1; else ix=-1;
-  for(x=x0;x!=x1;x+=ix)
-    PutPixel(s,x,y0,colour);
+  x_check = x1 - x0;
+  y_check = y1 - y0;
+  total_check = x_check - y_check;
   
-  if(y1>y0) iy=1; else iy=-1;
-  for(y=y0;y!=y1;y+=iy)
-    PutPixel(s,x1,y,colour);
+  // if the y change is zero, draw a horizontal line
+  if(y_check == 0){
+    if(x1>x0) ix=1; else ix=-1;
+    for(x=x0;x!=x1;x+=ix)
+      PutPixel(s,x,y0,colour);
+  }
+  
+  // if the x change is zero, draw a vertical line
+  else if(x_check == 0){
+    if(y1>y0) iy=1; else iy=-1;
+    for(y=y0;y!=y1;y+=iy)
+      PutPixel(s,x1,y,colour); 
+  }
+  
+  // if the x change is greater than zero, draw to the right
+  else if(x_check > 0){
+    // if the y change is greater than zero, draw down
+    if(y_check > 0){
+      
+    }
+    
+    
+    // if the y change is greater than zero, draw up
+    if(y_check < 0){
+      for(x=x0, y=y0;x!=x1;x+=1){
+        PutPixel(s,x,y,colour);
+        if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) >= 0)
+          y--;
+      }
+    }
+  }
+  
+  // if the x change is smaller than zero, draw to the left
+  else if(x_check < 0){
+    // if the y change is greater than zero, draw down
+    if(y_check > 0){
+      
+    }
+    
+    
+    // if the y change is greater than zero, draw up
+    if(y_check < 0){
+    
+    }
+  }
+  
 
   return;
 }
