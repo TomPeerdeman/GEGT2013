471e7dd9c65d06587f760a864925c0ab40bd8fc8
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-02-05 22:33:41 +0100

    Refactor complete, comments needed?

--- assignment1/basic_midpoint/mla.c
+++ assignment1/basic_midpoint/mla.c
@@ -5,67 +5,72 @@
  * Created by ...... Jurgen Sturm 
  *
  * Student names: Tom Peerdeman & Rene Aparicio Saez
- * Student emails: rene.apariciosaez@student.uva.nl
+ * Student emails: rene.apariciosaez@student.uva.nl & tom.peerdeman@student.uva.nl
  * Collegekaart 10266186 & 10214054
  * Date 05 Feb 2013
- * Comments ........
  *
- *
- * (always fill in these fields before submitting!!)
  */
 
 #include "SDL.h"   
 #include "init.h"
 
-/*
- * Midpoint Line Algorithm
- *
- * As you probably will have figured out, this is the part where you prove
- * your programming skills. The code in the mla function should draw a direct 
- * line between (x0,y0) and (x1,y1) in the specified color. 
- * 
- * Until now, the example code below draws only a horizontal line between
- * (x0,y0) and (x1,y0) and a vertical line between (x1,y1).
- * 
- * And here the challenge begins..
- *
- * Good luck!
- *
- *
- */
- 
-void mla_func(SDL_Surface *s, Uint32 colour, int x0, int y0, int x1, int y1,
-		int loop_x, int check_lt) {
+void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
+	// Variables for the function
+	int x_check;
+	int y_check;
+	int total_check;
+
 	int i, j;
-	
+
 	int func;
 	int func_inc;
 	int func_cond_inc;
 	int inc_cond;
 	int loop_inc;
 	int loop_max;
+
+	int loop_x;
+	int check_lt;
+
+	// Initialize checks
+	x_check = x1 - x0;
+	y_check = y1 - y0;
+	total_check = abs(x_check) - abs(y_check);
+
+	// Loop in x or y direction, depends on the total_check
+	loop_x = (total_check > 0) ? 1 : 0;
+
+	// The condition in the loop should check for a function value less than zero
+	// if 
+	if((y_check >= 0 && x_check > 0) || (x_check <= 0 && y_check <= 0)) {
+		check_lt = loop_x;
+	} else {
+		check_lt = !loop_x;
+	}
 	
+	// Initial function value
 	func = (y0-y1) * x0 + (x1-x0) * y0 + x0 * y1 - x1 * y0;
 	
+	// The properties of the loop depend of we loop in x or y direcion
 	if(loop_x) {
 		func_inc = (y0-y1);
-		loop_inc = (x1 - x0 > 0) ? 1 : -1;
+		loop_inc = (x_check > 0) ? 1 : -1;
 		func_inc *= loop_inc;
 		
 		func_cond_inc = (x1-x0);
-		inc_cond = (y1 - y0 > 0) ? 1 : -1;
+		inc_cond = (y_check > 0) ? 1 : -1;
 		func_cond_inc *= inc_cond;
 		
 		loop_max = x1;
-        i = x0;
-        j = y0;
+		i = x0;
+		j = y0;
 	} else {
-        func_inc = (x1-x0);
-		loop_inc = (y1 - y0 > 0) ? 1 : -1;
+		func_inc = (x1-x0);
+		loop_inc = (y_check > 0) ? 1 : -1;
 		func_inc *= loop_inc;
 		
 		func_cond_inc = (y0-y1);
-		inc_cond = (x1 - x0 > 0) ? 1 : -1;
+		inc_cond = (x_check > 0) ? 1 : -1;
 		func_cond_inc *= inc_cond;
 		
 		loop_max = y1;
@@ -73,138 +78,21 @@ void mla_func(SDL_Surface *s, Uint32 colour, int x0, int y0, int x1, int y1,
 		j = x0;
 	}
 	
+	// The actual loop that sets the pixel values.
+	// Depending on the loop_x bool this loops over x or y, i is therefore x or y
+	// j is the opposite direction of i
 	for(; i != loop_max; i += loop_inc) {
 		if(loop_x)
 			PutPixel(s, i, j, colour);
-		else 
+		else
 			PutPixel(s, j, i, colour);
-			
+		
+		// Instead of recalculating the function just increase the function value
 		func += func_inc;
 		
 		if((check_lt && func < 0) || (!check_lt && func >= 0)) {
 			func += func_cond_inc;
 			j += inc_cond;
-		}
-	}
-} 
-
-void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
-	// variables for the function
-	int x,y;
-	int x_check;
-	int y_check;
-	int total_check;
-
-	// initialize checks
-	x_check = x1 - x0;
-	y_check = y1 - y0;
-	total_check = abs(x_check) - abs(y_check);
-
-	// set buffers
-	x=x0;
-	y=y0;
-
-	// if the x change is greater than zero, draw to the right
-	if(x_check > 0){
-	
-		// if the y change is greater than zero, draw down
-		if(y_check >= 0){
-		
-			// draw red octant
-			if(total_check > 0){
-				// for(;x!=x1;x+=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-						// y++;
-				// }
-				mla_func(s, colour, x0, y0, x1, y1, 1, 1);
-				
-			}
-
-			// draw green octant
-			else{
-				// for(;y!=y1;y+=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) >= 0)
-						// x++;
-				// }
-				mla_func(s, colour, x0, y0, x1, y1, 0, 0);
-			}
-		}
-
-		// if the y change is smaller than zero, draw up
-		else if(y_check < 0){
-		
-			// draw white octant
-			if(total_check > 0){
-				// for(;x!=x1;x+=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
-						// y--;
-				// }
-				mla_func(s, colour, x0, y0, x1, y1, 1, 0);
-			}
-
-			// draw blue octant
-			else{
-				// for(;y!=y1;y-=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-						// x++;
-				// }    
-				mla_func(s, colour, x0, y0, x1, y1, 0, 1);
-			}
-		}
-	}
-	// if the x change is smaller than zero, draw to the left
-	else if(x_check <= 0){
-		
-		// if the y change is greater than zero, draw down
-		if(y_check > 0){
-
-			// draw white octant
-			if(total_check >= 0){
-				// for(;x!=x1;x-=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
-						// y++;
-				// }
-				mla_func(s, colour, x0, y0, x1, y1, 1, 0);
-			}
-
-			// draw blue octant
-			else{
-				// for(;y!=y1;y+=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-						// x--;
-				// }
-				mla_func(s, colour, x0, y0, x1, y1, 0, 1);
-			}
-		}
-
-		// if the y change is greater than zero, draw up
-		else if(y_check <= 0){
-
-			// draw red octant
-			if(total_check > 0){
-				// for(;x!=x1;x-=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-						// y--;
-				// }
-				mla_func(s, colour, x0, y0, x1, y1, 1, 1);
-			}
-
-			// draw green octant
-			else{
-				// for(;y!=y1;y-=1){
-					// PutPixel(s,x,y,colour);
-					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
-						// x--;
-				// }
-				mla_func(s, colour, x0, y0, x1, y1, 0, 0);
-			}
 		}
 	}
 }
