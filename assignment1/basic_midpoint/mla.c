23015db1df1e9f70ba2d9a0da75d9cffd347143a
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-02-05 19:33:35 +0000

    Refactor almost complete

--- assignment1/basic_midpoint/mla.c
+++ assignment1/basic_midpoint/mla.c
@@ -33,117 +33,178 @@
  *
  *
  */
+ 
+void mla_func(SDL_Surface *s, Uint32 colour, int x0, int y0, int x1, int y1,
+		int loop_x, int check_lt) {
+	int i, j;
+	
+	int func;
+	int func_inc;
+	int func_cond_inc;
+	int inc_cond;
+	int loop_inc;
+	int loop_max;
+	
+	func = (y0-y1) * x0 + (x1-x0) * y0 + x0 * y1 - x1 * y0;
+	
+	if(loop_x) {
+		func_inc = (y0-y1);
+		loop_inc = (x1 - x0 > 0) ? 1 : -1;
+		func_inc *= loop_inc;
+		
+		func_cond_inc = (x1-x0);
+		inc_cond = (y1 - y0 > 0) ? 1 : -1;
+		func_cond_inc *= inc_cond;
+		
+		loop_max = x1;
+        i = x0;
+        j = y0;
+	} else {
+        func_inc = (x1-x0);
+		loop_inc = (y1 - y0 > 0) ? 1 : -1;
+		func_inc *= loop_inc;
+		
+		func_cond_inc = (y0-y1);
+		inc_cond = (x1 - x0 > 0) ? 1 : -1;
+		func_cond_inc *= inc_cond;
+		
+		loop_max = y1;
+		i = y0;
+		j = x0;
+	}
+	
+	for(; i != loop_max; i += loop_inc) {
+		if(loop_x)
+			PutPixel(s, i, j, colour);
+		else 
+			PutPixel(s, j, i, colour);
+			
+		func += func_inc;
+		
+		if((check_lt && func < 0) || (!check_lt && func >= 0)) {
+			func += func_cond_inc;
+			j += inc_cond;
+		}
+	}
+} 
+
 void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
-  // variables for the function
-  int x,y;
-  int x_check;
-  int y_check;
-  int total_check;
-  
-  // initialize checks
-  x_check = x1 - x0;
-  y_check = y1 - y0;
-  total_check = abs(x_check) - abs(y_check);
-  
-  // set buffers
-  x=x0;
-  y=y0;
+	// variables for the function
+	int x,y;
+	int x_check;
+	int y_check;
+	int total_check;
 
-  
-  // if the x change is greater than zero, draw to the right
-  if(x_check > 0){
-    
-    // if the y change is greater than zero, draw down
-    if(y_check >= 0){
-      
-      // draw red octant
-      if(total_check > 0){
-        for(;x!=x1;x+=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-            y++;
-        }
-      }
-      
-      // draw green octant
-      else{
-        for(;y!=y1;y+=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) >= 0)
-            x++;
-        }
-      }
-    }
-    
-    
-    // if the y change is smaller than zero, draw up
-    else if(y_check < 0){
-      
-      // draw white octant
-      if(total_check > 0){
-        for(;x!=x1;x+=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
-            y--;
-        }
-      }
-      
-      // draw blue octant
-      else{
-        for(;y!=y1;y-=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-            x++;
-        }      
-      }
-    }
-  }
-  // if the x change is smaller than zero, draw to the left
-  else if(x_check <= 0){
-    // if the y change is greater than zero, draw down
-    if(y_check > 0){
-      
-      // draw white octant
-      if(total_check >= 0){
-        for(;x!=x1;x-=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
-            y++;
-        }
-      }
-      
-      // draw blue octant
-      else{
-        for(;y!=y1;y+=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-            x--;
-        }
-      }
-    }
-    
-    // if the y change is greater than zero, draw up
-    else if(y_check <= 0){
-      
-      // draw red octant
-      if(total_check > 0){
-        for(;x!=x1;x-=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
-            y--;
-        }
-      }
-      
-      // draw green octant
-      else{
-        for(;y!=y1;y-=1){
-          PutPixel(s,x,y,colour);
-          if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
-            x--;
-        }
-      }
-    }
-  }
-  return;
+	// initialize checks
+	x_check = x1 - x0;
+	y_check = y1 - y0;
+	total_check = abs(x_check) - abs(y_check);
+
+	// set buffers
+	x=x0;
+	y=y0;
+
+	// if the x change is greater than zero, draw to the right
+	if(x_check > 0){
+	
+		// if the y change is greater than zero, draw down
+		if(y_check >= 0){
+		
+			// draw red octant
+			if(total_check > 0){
+				// for(;x!=x1;x+=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
+						// y++;
+				// }
+				mla_func(s, colour, x0, y0, x1, y1, 1, 1);
+				
+			}
+
+			// draw green octant
+			else{
+				// for(;y!=y1;y+=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) >= 0)
+						// x++;
+				// }
+				mla_func(s, colour, x0, y0, x1, y1, 0, 0);
+			}
+		}
+
+		// if the y change is smaller than zero, draw up
+		else if(y_check < 0){
+		
+			// draw white octant
+			if(total_check > 0){
+				// for(;x!=x1;x+=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
+						// y--;
+				// }
+				mla_func(s, colour, x0, y0, x1, y1, 1, 0);
+			}
+
+			// draw blue octant
+			else{
+				// for(;y!=y1;y-=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
+						// x++;
+				// }    
+				mla_func(s, colour, x0, y0, x1, y1, 0, 1);
+			}
+		}
+	}
+	// if the x change is smaller than zero, draw to the left
+	else if(x_check <= 0){
+		
+		// if the y change is greater than zero, draw down
+		if(y_check > 0){
+
+			// draw white octant
+			if(total_check >= 0){
+				// for(;x!=x1;x-=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
+						// y++;
+				// }
+				mla_func(s, colour, x0, y0, x1, y1, 1, 0);
+			}
+
+			// draw blue octant
+			else{
+				// for(;y!=y1;y+=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
+						// x--;
+				// }
+				mla_func(s, colour, x0, y0, x1, y1, 0, 1);
+			}
+		}
+
+		// if the y change is greater than zero, draw up
+		else if(y_check <= 0){
+
+			// draw red octant
+			if(total_check > 0){
+				// for(;x!=x1;x-=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) < 0)
+						// y--;
+				// }
+				mla_func(s, colour, x0, y0, x1, y1, 1, 1);
+			}
+
+			// draw green octant
+			else{
+				// for(;y!=y1;y-=1){
+					// PutPixel(s,x,y,colour);
+					// if(((y0-y1) * x + (x1-x0) * y + x0 * y1 - x1 * y0) > 0)
+						// x--;
+				// }
+				mla_func(s, colour, x0, y0, x1, y1, 0, 0);
+			}
+		}
+	}
 }
-
