601cf85b7ae33adda43d973051e7658cbd34189c
Rene <rene66613@hotmail.com>
2013-02-18 20:00:26 +0100

    it seems to be working, try spacebar

--- assignment3/orthogonal_projection/ortho.c
+++ assignment3/orthogonal_projection/ortho.c
@@ -17,6 +17,7 @@
 #include <GL/glut.h>   
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <stdio.h>
 
  
 #define sqr(x) ((x)*(x))
@@ -32,5 +33,38 @@ void myOrtho(GLdouble left,
              GLdouble top,
              GLdouble near,
              GLdouble far) {
+	
+	// variables
+	double lrplus, rlmin, btplus, tbmin, nfplus, nfmin;
 
+	// set all variables outside the matrices
+	lrplus = (left+right)/2;
+	btplus = (bottom+top)/2;
+	nfplus = (near+far)/2;
+	rlmin = 2/(right-left);
+	tbmin = 2/(top-bottom);
+	nfmin = 2/(near-far);
+
+	printf("%lf ; %lf ; %lf\n", lrplus, btplus, nfplus);
+	printf("%lf ; %lf ; %lf\n", rlmin, tbmin, nfmin);
+
+	// the first part of the orthogonal projection
+	GLfloat MO1[16] = {
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		-lrplus, -btplus, -nfplus, 1
+	};
+
+	// second part of the orthogonal projection
+	GLfloat MO2[16] = {
+		rlmin, 0, 0, 0,
+		0, tbmin, 0, 0,
+		0, 0, nfmin, 0,
+		0, 0, 0, 1
+	};
+	
+	// apply the matrices
+	glMultMatrixf(MO1);
+	glMultMatrixf(MO2);
 }
