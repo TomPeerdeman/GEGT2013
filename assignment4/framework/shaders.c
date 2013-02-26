635f6b2bcb683773d0cbd34907bd7889366f84a8
Rene <rene66613@hotmail.com>
2013-02-26 19:35:31 +0100

    blinn phong working

--- assignment4/framework/shaders.c
+++ assignment4/framework/shaders.c
@@ -36,18 +36,19 @@ shade_constant(intersection_point ip)
 vec3
 shade_matte(intersection_point ip)
 {
-    // setup variables that will hold the dotproduct and intensity values
-    float ndotli;
-    // setup variable that will store the color that should be returned
-    float color = 0.0;
+    // setup variables that will hold the dotproduct and total color values
+    float ndotli, color = 0.0;
+    
     // setup the vector variable that will hold the vector from the
     // intersection to the light source
     vec3 li;
+    
     // loop over all the light sources
     for(int i = 0; i < scene_num_lights; i++){
         // get the intensity of the light source
         // calculate the vector from the intersection point to the light source
         li = v3_subtract(scene_lights[i].position,ip.p);
+        
         // normalize the vector, might be unnecessary?
         li = v3_normalize(li);
         
@@ -57,10 +58,12 @@ shade_matte(intersection_point ip)
             // calculate the dotproduct between the normal vector and the
             // vector towards the light source
             ndotli = v3_dotprod(ip.n, li);
-            // add the found value to total color if the value is positive
+            
+            // add the found value to the total color if the value is positive
             if(ndotli > 0.0)
                 color += (scene_ambient_light
                         + scene_lights[i].intensity*ndotli);
+            // else only add the ambient light value
             else
                 color += scene_ambient_light;
         }
@@ -72,7 +75,53 @@ shade_matte(intersection_point ip)
 vec3
 shade_blinn_phong(intersection_point ip)
 {
-    return v3_create(1, 0, 0);
+    // setup known variables,the start value of the sums for the color and a 
+    // variable that will hold the dot product of n with li.
+    float kd = 0.8,
+    ks = 0.2,
+    alpha = 50,
+    sum1 = 0.0,
+    sum2 = 0.0,
+    ndotli;
+    
+    // setup known vectors, and empty vectors that will hold values in the
+    // for-loops
+    vec3 cd = v3_create(1,0,0),
+    cs = v3_create(1,1,1),
+    li, h;
+    
+    // loop over all the light source
+    for(int i = 0; i < scene_num_lights; i++){
+        // get the intensity of the light source
+        // calculate the vector from the intersection point to the light source
+        li = v3_subtract(scene_lights[i].position,ip.p);
+        
+        // normalize the vector, might be unnecessary?
+        li = v3_normalize(li);
+        
+        // check for shadow, use a small offset to fix self-shading
+        // continue of the point is not a shadow
+        if(!shadow_check(v3_add(ip.p, v3_create(0.1,0.1,0.1)), li)){
+            // calculate the dotproduct between the normal vector and the
+            // vector towards the light source
+            ndotli = v3_dotprod(ip.n, li);
+            
+            // add the found value to the first if the value is positive
+            if(ndotli > 0.0)
+                sum1 += scene_lights[i].intensity*ndotli;
+                
+            // calculate the halfway point h, we use multiple because divide
+            // is not defined. therefore we divide 1 by the found length
+            h = v3_multiply(v3_add(ip.i,li), 1/v3_length(v3_add(ip.i,li)));
+            
+            // with this halfway point, calculate the value to be added to the
+            // second sum
+            sum2 += scene_lights[i].intensity*pow(v3_dotprod(ip.n,h),alpha);
+        }
+    }
+    // calculate the color that should be added
+    return v3_add(v3_multiply(cd, scene_ambient_light + kd*sum1),
+                   v3_multiply(cs, ks*sum2));
 }
 
 vec3
