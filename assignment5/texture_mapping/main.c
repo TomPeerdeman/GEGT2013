3ae7bac5e3c32251c9ef73fef4b5494dadd490a5
Rene <rene66613@hotmail.com>
2013-03-12 11:11:35 +0100

    mipmap comment

--- assignment5/texture_mapping/main.c
+++ assignment5/texture_mapping/main.c
@@ -277,13 +277,16 @@ InitGL(void)
              * GL_NEAREST
              * GL_LINEAR
              *
-             * not sure if im doing something wrong, but they are kinda ugly imo
+             * not sure if im doing something wrong,
+             * but they are kinda ugly imo
              */
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glCheckError("glTexParameteri");
             
             // Build MipMaps
+            // According to the manual this is the right call
+            // substracted from the old glTexImage2D call
             gluBuild2DMipmaps(GL_TEXTURE_2D, texture_internal_format,
                 width, height, texture_format, texture_type, image_data);
             //glTexImage2D(GL_TEXTURE_2D, 0, texture_internal_format,
