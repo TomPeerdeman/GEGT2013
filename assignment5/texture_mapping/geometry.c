120baa4ca327ccd801c229b452786098cf47cbed
Rene <rene66613@hotmail.com>
2013-03-12 11:08:12 +0100

    skydome working, tree cylinder aswell, mipmapping is doing something, not sure if its ok

--- assignment5/texture_mapping/geometry.c
+++ assignment5/texture_mapping/geometry.c
@@ -103,8 +103,12 @@ setHemispherePoint(vec3 *p, vec3* n, vec3* t, int latitude, int longitude,
     p->z = oz + cos(longitude * dToR) * cos(latitude * dToR) * s;
 
     // Set texture coordinate
-    t->x = 0.0;
-    t->y = 0.0;
+    // 1 time is for full sphere mapping
+    //t->x = (longitude * dToR) / (2*M_PI);
+    //t->y = (M_PI - (latitude * dToR)) / M_PI;
+    // dividing by 2 f-ed things up, therefore multiply
+    t->x = 2*(longitude * dToR) / (2*M_PI);
+    t->y = 2*(M_PI - (latitude * dToR)) / M_PI;
 
     // calculate normal
     n->x = p->x - ox;
@@ -198,7 +202,10 @@ createCylinder(polys * list, double radius, double height,
             p.normal[i].z /= len;
 
             // Set texture coordinate
-            p.tcoord[i].x = p.tcoord[i].y = 0.0;
+            //p.tcoord[i].x = p.tcoord[i].y = 0.0;
+            // I presume this is the right call.
+            p.tcoord[i].x = p.normal[i].x / 2 * M_PI;
+            p.tcoord[i].y = (M_PI - p.normal[i].y) / M_PI;
         }
 
         AddPolyToPolylist(list, p);
--- assignment5/texture_mapping/main.c
+++ assignment5/texture_mapping/main.c
@@ -267,12 +267,27 @@ InitGL(void)
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            /*
+             * Possible MipMaps:
+             * GL_NEAREST_MIPMAP_NEAREST
+             * GL_LINEAR_MIPMAP_NEAREST
+             * GL_NEAREST_MIPMAP_LINEAR
+             * GL_LINEAR_MIPMAP_LINEAR
+             * Other possibilities (not MipMap):
+             * GL_NEAREST
+             * GL_LINEAR
+             *
+             * not sure if im doing something wrong, but they are kinda ugly imo
+             */
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glCheckError("glTexParameteri");
-
-            glTexImage2D(GL_TEXTURE_2D, 0, texture_internal_format,
-                width, height, 0, texture_format, texture_type, image_data);
+            
+            // Build MipMaps
+            gluBuild2DMipmaps(GL_TEXTURE_2D, texture_internal_format,
+                width, height, texture_format, texture_type, image_data);
+            //glTexImage2D(GL_TEXTURE_2D, 0, texture_internal_format,
+                //width, height, 0, texture_format, texture_type, image_data);
             glCheckError("glTexImage2D");
 
             // Free the image data, as OpenGL will have made its internal copy by now
@@ -447,7 +462,7 @@ DrawGLScene(void)
 
     glPushAttrib(GL_LIGHTING_BIT);
     glDisable(GL_LIGHTING);
-    //DrawPolylist(polylistSkydome);
+    DrawPolylist(polylistSkydome);
     glPopAttrib();
 
     glutSwapBuffers();
