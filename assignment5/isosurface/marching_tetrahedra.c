a530e5023c0123028fb860de0bdcd377254cfe28
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-03-13 15:09:55 +0100

    First quad

--- assignment5/isosurface/marching_tetrahedra.c
+++ assignment5/isosurface/marching_tetrahedra.c
@@ -151,6 +151,26 @@ generate_tetrahedron_triangles(triangle *triangles, unsigned char isovalue, cell
     // case 0011 or 1100, quad -> 2 triangles
     if((!(v0_less) && !(v1_less) &&   v2_less  &&   v3_less) ||
        (  v0_less  &&   v1_less  && !(v2_less) && !(v3_less))){
+	   	vec3 vec02, vec03, vec13, vec12;
+		vec02 = interpolate_points(isovalue, c.p[v0], c.p[v2], val0, val2);
+		vec03 = interpolate_points(isovalue, c.p[v0], c.p[v3], val0, val3);
+		vec13 = interpolate_points(isovalue, c.p[v1], c.p[v3], val1, val3);
+		vec12 = interpolate_points(isovalue, c.p[v1], c.p[v2], val1, val2);
+		
+		triangles->p[0] = vec03;
+        triangles->p[1] = vec02;
+        triangles->p[2] = vec13;
+
+        triangle_calc_normal(triangles);
+        triangles++;
+		
+		triangles->p[0] = vec12;
+        triangles->p[1] = vec13;
+        triangles->p[2] = vec02;
+
+        triangle_calc_normal(triangles);
+        triangles++;
+		
       return 2;
     }
     
