277d5d02db3362b94d8d0f4a8b2e5f922f56e5d1
Rene <rene66613@hotmail.com>
2013-03-13 14:06:30 +0100

    new case added

--- assignment5/isosurface/marching_tetrahedra.c
+++ assignment5/isosurface/marching_tetrahedra.c
@@ -100,6 +100,18 @@ generate_tetrahedron_triangles(triangle *triangles, unsigned char isovalue, cell
     // case 0010 or 1101, 1 triangle
     if((!(v0_less) && !(v1_less) &&   v2_less  && !(v3_less)) ||
        (  v0_less  &&   v1_less  && !(v2_less) &&   v3_less)){
+
+      triangles->p[0] = interpolate_points(isovalue, c.p[v1], c.p[v3], val1, val3);
+      triangles->p[1] = interpolate_points(isovalue, c.p[v1], c.p[v2], val1, val2);
+      triangles->p[2] = interpolate_points(isovalue, c.p[v1], c.p[v0], val1, val0);
+
+      normal = v3_crossprod(v3_subtract(triangles->p[1], triangles->p[0]), v3_subtract(triangles->p[2], triangles->p[0]));
+
+      triangles->n[0] = normal;
+      triangles->n[1] = normal;
+      triangles->n[2] = normal;
+
+      triangles++;
     
       return 1;
     }
