9f7d131e2f286f1004b8d92e0b11736b8a5b35d8
René Aparicio Saez <10214054@u019919.fnwi.uva.nl>
2013-03-15 15:17:41 +0100

    mipmap update

--- assignment5/texture_mapping/main.c
+++ assignment5/texture_mapping/main.c
@@ -280,7 +280,7 @@ InitGL(void)
              * GL_LINEAR
              */
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glCheckError("glTexParameteri");
             
             // Build MipMaps
